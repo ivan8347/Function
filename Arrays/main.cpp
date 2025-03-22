15ab0b32a13db9e3dec974cb073c0c2f2c82dd04
kit <kit@DESKTOP-EHKDJE1>
2025-03-23 00:00:15 +0300

    Functon 1

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -4,67 +4,52 @@ using namespace std;
 using std::cin;
 using std::cout;
 using std::endl;
-void FILLRand(int arr[], const int size);
-void Print(int arr[], const int size);
-void Sort(int arr[], const int size);
+void FILLRand(int arr[], const int n);
+void Print(int arr[], const int n);
+void Sort(int arr[], const int n);
 void main()
 {
-
 	setlocale(LC_ALL, "");
-	const int size = 8;
-	int arr[size];
-	for (int i = 0; i < size; i++)
-	{
-		arr[i] = rand() % 100;
-	}
-	for (int i = 0; i < size; i++)
-	{
-		cout << arr[i] << "\t";
-	} cout << endl;
-	for (int i = 0; i < size; i++)
-	{
+	const int n = 5;
+	int arr[n];
+	FILLRand( arr, n);
+	Print(arr, n);
+	Sort(arr, n);
+	Print(arr, n);
 
-		for (int j = i + 1; i < size; i++)
-		{
-			if (arr[j] < arr[i]);
-			{
-				int buf = arr[i];
-				arr[i] = arr[j];
-				arr[j] = buf;
-			}
-			cout << arr[j] << "\t";
-		}
-	}for (int i = 0; i < size; i++)
-	{
-		cout << arr[i] << "\t";
-	} cout << endl;
-	
+	const int size = 8;
+	int brr[8];
+	FILLRand(brr, size);
+	Print(brr, size);
+	Sort(brr, size);
+	Print(brr, size);
 }
-void FILLRand(int arr[], const int size)
+void FILLRand(int arr[], const int n)
   {
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
 	}
   }
-  void Print(int arr[], const int size)
+void Print(int arr[], const int n)
   {
-	  for (int i = 0; i < size; i++)
+	  for (int i = 0; i < n; i++)
 	  {
 		  cout << arr[i] << "\t";
 	  } cout << endl;
   }
-  void Sort(int arr[], const int size) 
+  void Sort(int arr[], const int n) 
   {
-
-	  for (int j = i + 1; i < size; i++)
+	  for (int i = 0; i < n; i++)
 	  {
-		  if (arr[j] < arr[i]);
+		  for (int j = i + 1; j < n; j++)
 		  {
-			  int buf = arr[i];
-			  arr[i] = arr[j];
-			  arr[j] = buf;
+			  if (arr[j] < arr[i])
+			  {
+				  int buffer = arr[i];
+				  arr[i] = arr[j];
+				  arr[j] = buffer;
+			  }
 		  }
-		  
 	  }
   }
