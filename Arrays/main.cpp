d0a99a6e1481e812bfa4effad360007ade786a6a
kit <kit@DESKTOP-EHKDJE1>
2025-03-29 13:05:12 +0300

    Function Overload

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -2,19 +2,41 @@
 #include<conio.h>
 #define SORTING
 //#define HOME WORK
+#define delimiter "\n-----------------\n";
+const int ROWS = 3;
+const int COLS = 4;
 using namespace std;
 using std::cin;
 using std::cout;
 using std::endl;
 void FILLRand(int arr[], const int n);
+void FILLRand(double arr[], const int n);
+void FILLRand(int arr[ROWS][COLS], const int ROWS, const int COLS);
+
 void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+void Print(int arr[ROWS] [COLS], const int ROWS, const int COLS);
+
 void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+
 double Sum(int arr[], const int n);
+double Sum(double arr[], const int n);
+
 double Min(int arr[], const int n);
+double Min(double arr[], const int n);
+
 double Max(int arr[], const int n);
+double Max(double arr[], const int n);
+
 void Rev(int arr[], const int n);
+void Rev(double arr[], const int n);
+
 void ShiftLeft(int arr[], const int n,int tab);
+void ShiftLeft(double arr[], const int n,int tab);
+
 void ShiftRight(int arr[], const int n,int tab);
+void ShiftRight(double arr[], const int n,int tab);
 
 
 void main()
@@ -32,8 +54,8 @@ void main()
 	cout << "Максимальное значение массива :  " << Max(arr, n) << endl;
 	
 
-	const int size = 8;
-	int brr[8];
+	const int size = 8.2;
+double brr[8];
 	FILLRand(brr, size);
 	Print(brr, size);
 	Sort(brr, size);
@@ -68,16 +90,45 @@ void main()
 	cout << "Сдвиг массива вправо : ";
 	Print(arr, n);*/
 #endif // HOME WORK
+	cout << delimiter;
+
+
+int i_arr_2[ROWS][COLS]=
+{
+	{ 3, 5, 8 },
+	{ 13,21,34 },
+	{ 55,89,144 },
+};
+FILLRand(i_arr_2,ROWS,COLS);
+
+    Print(i_arr_2,ROWS,COLS);
+    
+
 }
-
-
-
   void FILLRand(int arr[], const int n)
   {
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
 	}
+  }
+  void FILLRand(double arr[], const int n)
+  {
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+  }
+  void FILLRand(int arr[ROWS][COLS], const int ROWS, const int COLS)
+  {
+	  for (int i = 0; i < ROWS; i++)
+	  {
+		  for (int j = 0; j < COLS; j++)
+		  {
+			  arr[i][j] = rand();
+		  }
+		  
+	  }
   }
   void Print(int arr[], const int n)
   {
@@ -86,7 +137,41 @@ void main()
 		  cout << arr[i] << " ";
 	  } cout << endl;
   }
+  void Print(double arr[], const int n)
+  {
+	  for (int i = 0; i < n; i++)
+	  {
+		  cout << arr[i] << " ";
+	  } cout << endl;
+  }
+  void Print(int arr[ROWS][COLS], const int ROWS, const int COLS)
+  {	  
+  for (int i = 0; i < ROWS; i++)
+  {
+
+
+	  for (int j = 0; j < COLS; j++)
+	  {
+		  cout << arr[i][j] << "\t";
+	  }
+	  cout << endl;
+  }
+  }
   void Sort(int arr[], const int n) 
+  {
+	  for (int i = 0; i < n; i++)
+	  {
+		  for (int j = i + 1; j < n; j++)
+		  {
+			  if (arr[j] < arr[i])
+			  {
+				  int buffer = arr[i];
+				  arr[i] = arr[j];
+				  arr[j] = buffer;
+			  }
+		  }
+	  }
+  }  void Sort(double arr[], const int n)
   {
 	  for (int i = 0; i < n; i++)
 	  {
@@ -109,6 +194,14 @@ void main()
 		   sum += arr[i];
 	  }cout << endl;
 	  return sum;
+  }double Sum(double arr[], const int n)
+  {
+	  double sum = 0;
+	  for (int i = 0; i < n; i++)
+	  {
+		   sum += arr[i];
+	  }cout << endl;
+	  return sum;
   }
   void Rev(int arr[], const int n) 
   {
@@ -116,8 +209,21 @@ void main()
 	  {
 		  cout << arr[i] << " ";
 	  }cout << endl;
+  }void Rev(double arr[], const int n)
+  {
+	  for (int i = n - 1; i >= 0; i--)
+	  {
+		  cout << arr[i] << " ";
+	  }cout << endl;
   }
   double Min(int arr[], const int n)
+  {
+	  int min = arr[0];
+	  for (int i = 0; i < n; i++)
+	  {
+		  if (min > arr[i]) min = arr[i];
+	  }return min;
+  } double Min(double arr[], const int n)
   {
 	  int min = arr[0];
 	  for (int i = 0; i < n; i++)
@@ -132,10 +238,17 @@ void main()
 	  {
 		  if (max < arr[i]) max = arr[i];
 	  }return max;
+  }  double Max(double arr[], const int n)
+  {
+	  int max = arr[0];
+	  for (int i = 0; i < n; i++)
+	  {
+		  if (max < arr[i]) max = arr[i];
+	  }return max;
   }
   void ShiftLeft(int arr[], const int n,int tab)
   {
-	  tab = tab % n;
+	 
 	  for (int i = 0; i < tab; i++)
 	  {
 		  int a = arr[0];
@@ -147,9 +260,36 @@ void main()
 		  arr[n - 1] = a;
 	  }	  
   }
-  void ShiftRight(int arr[], const int n, int tab) 
+  void ShiftLeft(double arr[], const int n, double tab)
+  {
+	 
+	  for (int i = 0; i < tab; i++)
+	  {
+		  int a = arr[0];
+
+		  for (int j = 0; j < n - 1; j++)
+		  {
+			  arr[j] = arr[j + 1];
+		  }
+		  arr[n - 1] = a;
+	  }	  
+  }
+  void ShiftRight(int arr[], const int n, int tab)
   {
 	  tab = tab % n;
+	  for (int i = 0; i < tab; i++)
+	  {
+		  int a = arr[n - 1];
+
+		  for (int j = n - 1; j > 0; j--)
+		  {
+			  arr[j] = arr[j - 1];
+		  }
+	  }
+  }
+		void ShiftRight(double arr[], const int n, double tab)
+  {
+	 // tab = tab % n;
 	for (int i = 0; i < tab; i++)
 	{
 		int a = arr[n-1];
