5e4a632bc6130a414893e9615ae11d3f09eb0817
kit <kit@DESKTOP-EHKDJE1>
2025-04-26 11:31:39 +0300

    staticLib DONE

---
+++ LIBChek/Main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<FILLRand.h>
+#include<Print.h>
+#include<constants.h>
+
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	FILLRand(arr, n);
+	Print(arr, n);
+	cout << "\n";
+	cout << "delimetr" << endl;
+
+	int i_arr_2[ROWS,COLS]; 
+	FILLRand(i_arr_2, ROWS, COLS);
+	Print(i_arr_2, ROWS, COLS);
+
+}
